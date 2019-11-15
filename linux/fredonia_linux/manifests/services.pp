c4a71984544f5a8faaa0e2b40bd22c80d1f02989
Patrick Kelley <kell3991@fredonia.edu>
2019-11-14 23:01:00 -0500

    The packages for the services all install properly in Ubuntu 18.04; however, http, samba and ntp servers are not starting as required to.  Users file works so far but needs to be extended for different levels of user and for groups.

--- linux/fredonia_linux/manifests/services.pp
+++ linux/fredonia_linux/manifests/services.pp
@@ -1,3 +1,76 @@
 class fredonia_linux::services () {
-  #Filler
+  #services to manage 
+  #optional(if time allows): add notifications
+  # to update log when services are run & track
+  # for debugging 
+  #Samba running and enabled
+  package {
+    ensure   => 'installed',
+    provider => 'apt',
+    before   => Service['samba'],
+  }
+  service { 'samba':
+    ensure => running,
+    enable => true,
+  }
+
+  #cron running and enabled
+  package {'cron':
+    ensure   => 'installed',
+    provider => 'apt',
+    before   => Service['cron'],
+  }
+  service { 'cron':
+    ensure => running,
+    enable => true,
+  }
+
+  #ssh running and enabled
+  package { 'ssh':
+    ensure   => 'installed',
+    provider => 'apt',
+    before   => Service['ssh']
+  }
+  service { 'ssh':
+    ensure => running,
+    enable => true,
+  }
+
+  #httpd running and enabled
+  package { 'snap':
+    ensure   => 'installed',
+    provider => 'apt',
+    before   => Exec['install http']
+  }
+  exec { 'install http':
+    command => 'sudo snap install http',
+    before  => Service['http'],
+    path    => '/usr/bin',
+  }
+  service { 'http':
+    ensure => running,
+    enable => true,
+  }
+
+  #ntp running and enabled
+  package {
+    ensure   => 'installed',
+    provider => 'apt',
+    before   => Service['sntp'],
+  }
+  service { 'sntp':
+    ensure => running,
+    enable => true,
+  }
+
+  #iptables stopped and disabled
+  package { 'iptables':
+    ensure   => 'installed',
+    provider => 'apt',
+    before   => Service['iptables'],
+  }
+  service { 'iptables':
+    ensure => stopped,
+    enable => false
+  }
 }
--- linux/fredonia_linux/manifests/users.pp
+++ linux/fredonia_linux/manifests/users.pp
@@ -1,3 +1,12 @@
-class fredonia_linux::users () {
-  #Filler
+#add a user to the system
+class fredonia_linux::users (String $user_name = 'guest') {
+  #generic user
+  unless $user_name == '' {
+    user { $user_name :
+      ensure     => present,
+      managehome => true,
+      password   => password_here,
+      ssh_key    => key_here,
+    }
+  }
 }
