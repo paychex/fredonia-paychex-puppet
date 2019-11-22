10359f8c48dd16c48ce485e7174554c086513085
root <root@yumapn0h2.paychex.com>
2019-11-21 23:40:48 -0500

    Adding cron for invoking puppet

--- linux/fredonia_linux/manifests/cron.pp
+++ linux/fredonia_linux/manifests/cron.pp
@@ -1,3 +1,8 @@
 class fredonia_linux::cron () {
-  #Filler
+  cron { 'Execute puppet every 5 minutes':
+    command => '/opt/puppetlabs/bin/puppet apply --modulepath=/opt/fredonia_linux /opt/fredonia_linux/manifests/init.pp',
+    user    => root,
+    hour    => '*',   
+    minute  => '*/5',
+  }
 }
