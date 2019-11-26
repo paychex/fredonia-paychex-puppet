04a7447947628bfe8f87073009442dee2bb1b1af
dakota2117 <dakota2117@gmail.com>
2019-11-26 19:06:54 +0000

    Added Time-Script.ps1 and puppet code for script

--- windows/fredonia_windows/manifests/scriptexecute.pp
+++ windows/fredonia_windows/manifests/scriptexecute.pp
@@ -1,3 +1,8 @@
 class fredonia_windows::scriptexecute () {
-  #Filler
+	$path = 'C:/Users/user/Desktop/Time-Script.ps1'
+	$powershell = 'C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoProfile -NoLogo -NonInteractive'
+	exec{ 'script_execution':
+	command => "$powershell $path"
+	}
 }
+include fredonia_windows::scriptexecute
