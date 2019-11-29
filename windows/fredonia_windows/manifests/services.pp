7744853edc31835e19c6cd43a9178cc6f8d58f9e
dakota2117 <dakota2117@gmail.com>
2019-11-29 21:01:29 +0000

    added code to services.pp

--- windows/fredonia_windows/manifests/services.pp
+++ windows/fredonia_windows/manifests/services.pp
@@ -1,3 +1,31 @@
 class fredonia_windows::services () {
-  #Filler
+	service { 'wuauserv':
+	ensure => running,
+	enable => true,
+	}
+	service { 'TrustedInstaller':
+	ensure => running,
+	enable => true,
+	}
+	service { 'msiserver':
+	ensure => running,
+	enable => true,
+	}
+	service { 'W32Time':
+	ensure => running,
+	enable => true,
+	}
+	service { 'MpsSvc':
+	ensure => running,
+	enable => true,
+	}
+	service { 'EventLog':
+	ensure => running,
+	enable => true,
+	}
+	service { 'RemoteRegistry':
+	ensure => stopped,
+	enable => false,
+	}
 }
+class{'fredonia_windows::services':}
