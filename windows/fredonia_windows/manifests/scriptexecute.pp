56bcf533715b30b11d52b1078f1178e6074a7b26
dakota2117 <dakota2117@gmail.com>
2019-11-29 18:43:12 +0000

    Ensured file was present

--- windows/fredonia_windows/manifests/scriptexecute.pp
+++ windows/fredonia_windows/manifests/scriptexecute.pp
@@ -1,6 +1,11 @@
 class fredonia_windows::scriptexecute () {
 	$path = 'C:/Users/user/Desktop/Time-Script.ps1'
 	$powershell = 'C:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -NoProfile -NoLogo -NonInteractive'
+	
+	file{'C:\\fredonia-windows\\windows\\Time-Script.txt':
+	ensure => 'present',
+	}
+	
 	exec{ 'script_execution':
 	command => "$powershell $path"
 	}
