4efafe476cc52e98f50dd7bc58703885bbc3e3da
dakota2117 <dakota2117@gmail.com>
2019-11-30 22:55:31 +0000

    added states to services

--- windows/fredonia_windows/manifests/services.pp
+++ windows/fredonia_windows/manifests/services.pp
@@ -1,36 +1,43 @@
 class fredonia_windows::services () {
-	service { 'wuauserv':
-	ensure => running,
-	enable => true,
-	}
 	registry_value { 'HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\wuauserv\\DelayedAutostart':
 	ensure => present,
 	type => dword,
 	data => 0x00000001
 	}
+	service { 'wuauserv':
+	ensure => running,
+	enable => true,
+	}
+
 	service { 'TrustedInstaller':
 	ensure => running,
 	enable => true,
+	start => "Automatic",
 	}
 	service { 'msiserver':
 	ensure => running,
 	enable => true,
+	start => "Automatic",
 	}
 	service { 'W32Time':
 	ensure => running,
 	enable => true,
+	start => "Automatic",
 	}
 	service { 'MpsSvc':
 	ensure => running,
 	enable => true,
+	start => "Automatic",
 	}
 	service { 'EventLog':
 	ensure => running,
 	enable => true,
+	start => "Automatic",
 	}
 	service { 'RemoteRegistry':
 	ensure => stopped,
 	enable => false,
+	start => "Disabled",
 	}
 }
-#class{'fredonia_windows::services':}
+class{'fredonia_windows::services':}
